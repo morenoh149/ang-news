bfb053062995b6e8987b2a111c120a9c758079a9
Harry Moreno <morenoh149@gmail.com>
2014-02-21 22:21:33 -0800

    added user model, chap 7

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -3,6 +3,19 @@
 app.controller('NavCtrl', function ($scope, $location, Post, Auth) {
   $scope.post = { url: 'http://', title: ''};
 
+  $scope.submitPost = function () {
+    Post.create($scope.post).then(function (postId) {
+      $scope.post = { url: 'http://', title: ''};
+      $location.path('/posts/' + postId);
+    });
+  };
+
+  $scope.logout = function () {
+    Auth.logout();
+  };
+});
+
+
 //  $scope.submitPost = function () {
 //    var urlPrefix = 'http://';
 //    if ($scope.post.url.toString().slice(0,7) !== urlPrefix) {
@@ -14,13 +27,4 @@ app.controller('NavCtrl', function ($scope, $location, Post, Auth) {
 //      });
 //    }
 //  };
-  $scope.submitPost = function () {
-    Post.create($scope.post).then(function (ref) {
-      $location.path('/posts/' + ref.naem());
-      $scope.post = { url: 'http://', title: ''};
-    });
-  };
-  $scope.logout = function () {
-    Auth.logout();
-  };
-});
+
--- app/scripts/services/post.js
+++ app/scripts/services/post.js
@@ -1,20 +1,42 @@
 'use strict';
 
 app.factory('Post',
-  function($firebase, FIREBASE_URL) {
+  function($firebase, FIREBASE_URL, User) {
     var ref = new Firebase(FIREBASE_URL + 'posts');
     var posts = $firebase(ref);
 
     var Post = {
       all: posts,
       create: function (post) {
-        return posts.$add(post);
+        if (User.signedIn()) {
+          var user = User.getCurrent();
+
+          post.owner = user.username;
+
+          return posts.$add(post).then(function (ref) {
+            var postId = ref.name();
+
+            user.$child('posts').$child(postId).$set(postId);
+
+            return postId;
+          });
+        }
       },
       find: function (postId) {
         return posts.$child(postId);
       },
       delete: function (postId) {
-        return posts.$remove(postId);
+        if (User.signedIn()) {
+          var post = Post.find(postId);
+
+          post.$on('loaded', function () {
+            var user = User.findByUsername(post.owner);
+
+            posts.$remove(postId).then(function () {
+              user.$child('posts').$remove(postId);
+            });
+          });
+        }
       }
     };
     return Post;
